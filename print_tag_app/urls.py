292d123c5b7997011319f88eb94e2a6973852f77
Weerawit Phumphuang <weerawit.pm@gmail.com>
2024-08-29 10:46:48 +0700

    update: product page

--- print_tag_app/urls.py
+++ print_tag_app/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from print_tag_app import views
+
 # from .views import get_modal_data
 
 urlpatterns = [
     path("", views.index),
-    path("invoice/<str:fcskid>/", views.get_invoice_data, name='get_invoice_data'),
-    path('autocomplete', views.autocomplete, name='autocomplete'),
-    path('save_selected', views.save_selected, name='save_selected'),
+    path("invoice/<str:fcskid>/", views.get_invoice_data, name="get_invoice_data"),
+    path("autocomplete", views.autocomplete, name="autocomplete"),
+    path("save_selected", views.save_selected, name="save_selected"),
     # path('get_modal_data/<str:fcskid>/', get_modal_data, name='get_modal_data'),
-    path('upload_packing/', views.upload_packing, name='upload_packing'),
+    path("upload_packing/", views.upload_packing, name="upload_packing"),
+    path("product", views.product, name="product"),
+    path("save_product", views.save_product, name="save_product"),
 ]
--- print_tag_app/views.py
+++ print_tag_app/views.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from .form import UploadFileForm
 from django.contrib import messages
 import pandas as pd
+import json
 
 
 @csrf_exempt
@@ -355,6 +356,79 @@ def upload_packing(request):
     else:
         form = UploadFileForm()
     return render(request, "upload_packing/index.html", {"form": form})
+
+
+def product(request):
+    with connections["formula_aaa"].cursor() as cursor:
+        query = """
+                Select TOP 1000 FCCODE, FCNAME
+                from PROD
+                """
+        cursor.execute(query)
+        data = cursor.fetchall()
+    return render(request, "product/index.html", {"data": data})
+
+
+def save_product(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            cust_sup = data.get("cust_sup")
+            selected_items = data.get("selected_items", [])
+
+            if not cust_sup or not selected_items:
+                return JsonResponse({"error": "Missing required data"}, status=400)
+
+            ref_tag = RefTag.objects.create()
+            for item in selected_items:
+                part_no = item.get(f"part_no")
+                part_name = item.get(f"part_name")
+                qr_code = f"{part_no}${part_name}${cust_sup}"
+
+                for _ in range(6):  # สร้าง 6 tags สำหรับแต่ละ part
+                    tag = Tag.objects.create(
+                        qr_code=qr_code,
+                        cust_sup=cust_sup,
+                        part_no=part_no,
+                        part_name=part_name,
+                        ref_tag=ref_tag,
+                    )
+
+            # URL สำหรับล็อกอิน
+            login_url = "http://192.168.20.16:8080/jasperserver/rest_v2/login?j_username=jasperadmin&j_password=jasperadmin"
+            login_response = requests.get(login_url)
+
+            if login_response.status_code == 200:
+                cookies = login_response.cookies
+
+                # URL ของรายงาน
+                report_url = f"http://192.168.20.16:8080/jasperserver/rest_v2/reports/aaa_report/tags.pdf?ParmID={ref_tag}"
+                report_response = requests.get(report_url, cookies=cookies)
+
+                if report_response.status_code == 200:
+                    file_name = f"report_{cust_sup}.pdf"
+                    response = HttpResponse(
+                        report_response.content, content_type="application/pdf"
+                    )
+                    response["Content-Disposition"] = (
+                        f'attachment; filename="{file_name}"'
+                    )
+                    return response
+                else:
+                    return JsonResponse(
+                        {"error": "Failed to retrieve the report"},
+                        status=report_response.status_code,
+                    )
+            else:
+                return JsonResponse(
+                    {"error": "Login to JasperServer failed"},
+                    status=login_response.status_code,
+                )
+
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+
+    return JsonResponse({"error": "Invalid request method"}, status=405)
 
 
 # def get_modal_data(request, fcskid):
